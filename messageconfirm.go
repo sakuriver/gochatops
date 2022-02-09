dfdab70fd8ac4c398386c7c1ed60f4c86f8ab010
sakuriver <suzuerain@yahoo.co.jp>
2022-02-09 21:42:25 +0900

    コマンド整備中

--- messageconfirm.go
+++ messageconfirm.go
@@ -9,21 +9,40 @@ import (
 
 func main() {
 	fmt.Printf("hello, world\n")
-	postTest()
-}
-
-func postTest() {
 	flag.Parse()
 	var flags = flag.Args()
+
+	if flags[5] == "1" {
+		postMessage(flags)
+	} else {
+		getMessage(flags)
+	}
+}
+
+func postMessage(flags []string) {
+	flag.Parse()
 	var postMessage = "body=お疲れ様です。+時間帯ですが、%[1]s時から%[2]s時でいかがでしょうか。場所は%[3]sがいいかと思います"
 	requestBody := fmt.Sprintf(postMessage, flags[0], flags[1], flags[2])
+	var url = fmt.Sprintf("https://api.chatwork.com/v2/rooms/%[1]s/messages", flags[3])
+	execChatWork(flags, "POST", url, requestBody)
+}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://api.chatwork.com/v2/rooms/%[1]s/messages", flags[3]), bytes.NewBufferString(requestBody))
+func getMessage(flags []string) {
+	flag.Parse()
+	var url = fmt.Sprintf("https://api.chatwork.com/v2/rooms/%[1]s/messages/%[2]s", flags[3], flags[0])
+	execChatWork(flags, "GET", url, "")
+	println(url)
+}
+
+func execChatWork(flags []string, method string, url string, requestBody string) {
+
+	req, err := http.NewRequest(method, url, bytes.NewBufferString(requestBody))
 	req.Header.Add("X-ChatWorkToken", flags[4])
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	resp, _ := http.DefaultClient.Do(req)
 	resp.Body.Close()
 	if err != nil {
 	}
-	fmt.Println(resp)
+	fmt.Println(resp.Request.Body)
+
 }
